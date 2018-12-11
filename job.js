00107c4a9636c25e00a8a1ce5d96b08de2a967cc
qq1728744610 <qq1728744610@users.noreply.github.com>
2018-12-11 16:43:50 +0800

    Update job.js

--- job.js
+++ job.js
@@ -1,6 +1,22 @@
-function sendMsg(){
-    postMessage(0);
-}
-sendMsg();
-//joyuweb.addGroup('group0', 0, 0, 1280, 720)
-//joyuweb.addImgView('group0', 'img0', 0, 0, -2, -2, 'http://112.50.251.22:8090/assets/img/poster/recommend/ddd1.png')
+         startTest()
+
+		 function startTest(){
+            for(var i = 0; i < 10000 ; i++){
+                postMessage(i)
+                sleep(50);
+            }
+        }
+
+        /**
+        *  睡眠函数
+        *  @param numberMillis -- 要睡眠的毫秒数
+        */
+        function sleep(numberMillis) {
+            var now = new Date();
+            var exitTime = now.getTime() + numberMillis;
+            while (true) {
+                now = new Date();
+                if (now.getTime() > exitTime)
+                    return;
+                }
+        }

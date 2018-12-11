e19ee25a559784cf740539a1edb237b771d3c288
qq1728744610 <qq1728744610@users.noreply.github.com>
2018-12-11 16:08:04 +0800

    Add files via upload

---
+++ job.js
@@ -0,0 +1,6 @@
+function sendMsg(){
+    postMessage(0);
+}
+sendMsg();
+//joyuweb.addGroup('group0', 0, 0, 1280, 720)
+//joyuweb.addImgView('group0', 'img0', 0, 0, -2, -2, 'http://112.50.251.22:8090/assets/img/poster/recommend/ddd1.png')

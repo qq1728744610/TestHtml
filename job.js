6e2d9436c2283e4f523d2ec0546c800bed7c9e0a
qq1728744610 <qq1728744610@users.noreply.github.com>
2018-12-11 16:46:13 +0800

    Update job.js

--- job.js
+++ job.js
@@ -1,22 +1,23 @@
-         startTest()
+startTest()
 
-		 function startTest(){
-            for(var i = 0; i < 10000 ; i++){
-                postMessage(i)
-                sleep(50);
-            }
-        }
+function startTest(){
+    for(var i = 0; i < 10000 ; i++){
+        postMessage(i)
+        sleep(50);
+    }
+ }
 
-        /**
-        *  睡眠函数
-        *  @param numberMillis -- 要睡眠的毫秒数
-        */
-        function sleep(numberMillis) {
-            var now = new Date();
-            var exitTime = now.getTime() + numberMillis;
-            while (true) {
-                now = new Date();
-                if (now.getTime() > exitTime)
-                    return;
-                }
-        }
+/**
+ *  睡眠函数
+ *
+ *  @param numberMillis -- 要睡眠的毫秒数
+ */
+function sleep(numberMillis) {
+     var now = new Date();
+     var exitTime = now.getTime() + numberMillis;
+      while (true) {
+         now = new Date();
+         if (now.getTime() > exitTime)
+             return;
+         }
+ }
